5d413ce7f77fc19123b123b321ed972bee5e0f6a
LuizFelixDev <luizhenriquefelix138@gmail.com>
2026-08-25 10:14:19 -0300

    chore(front): remove manual service worker registration in main.tsx

--- front-alusert/src/main.tsx
+++ front-alusert/src/main.tsx
@@ -8,11 +8,3 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then(reg => console.log('Service Worker registrado:', reg.scope))
-      .catch(err => console.log('Erro ao registrar Service Worker:', err));
-  });
-}
